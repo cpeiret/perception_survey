3c3320f533f10edc7c881b1d36c1e537962638b5
cpeiret <c.peiret-garcia2@newcastle.ac.uk>
2021-11-26 12:04:00 +0000

    clean data

--- perception_survey/munge/01_clean_data.R
+++ perception_survey/munge/01_clean_data.R
@@ -3,30 +3,30 @@
 clean_data = raw_data
 
 # keep needed columns
-variables = lapply(clean_data[colnames(clean_data)], var_lab) # see what each column means
-clean_data = clean_data[,c(8,11:13,22,33,39,55,60:62,66:98, 137:141)] # keep only needed columns
+variables = lapply(raw_data[colnames(raw_data)], var_lab) # see what each column means
+clean_data = clean_data[,c(8,11:13,22,33,39,55,57,60:62,66:98, 137:141)] # keep only needed columns
 variables_clean = lapply(clean_data[colnames(clean_data)], var_lab) # see what each column means
 
 # remove DK/NA values from satisfaction questions
-clean_data[,4:7] = dplyr::na_if(clean_data[,4:7], 5) # set DK/NA values as NA
+clean_data[,4:7] = dplyr::na_if(clean_data[,4:7], "DK/NA  (DO NOT READ OUT)") # set DK/NA values as NA
 
 # remove DK/NA values from demographic questions
-clean_data$cities = dplyr::na_if(clean_data$cities, 84) # set DK/NA values as NA
-clean_data$d14 = dplyr::na_if(clean_data$d14, 4) # set DK/NA values as NA
-clean_data$d1 = dplyr::na_if(clean_data$d1, 99) # set DK/NA values as NA
-clean_data$d1r1 = dplyr::na_if(clean_data$d1r1, 7) # set DK/NA values as NA
-clean_data$d1r2 = dplyr::na_if(clean_data$d1r2, 7) # set DK/NA values as NA
-clean_data$d4 = dplyr::na_if(clean_data$d4, 0) # set DK/NA values as NA
-clean_data$d4 = dplyr::na_if(clean_data$d4, 99) # set DK/NA values as NA
-clean_data$d4r1 = dplyr::na_if(clean_data$d4r1, 97) # set DK/NA values as NA
-clean_data$d4r1 = dplyr::na_if(clean_data$d4r1, 98) # set DK/NA values as NA
-clean_data$d4r2 = dplyr::na_if(clean_data$d4r2, 6) # set DK/NA values as NA
-clean_data$d4r2 = dplyr::na_if(clean_data$d4r2, 7) # set DK/NA values as NA
-clean_data$d5 = dplyr::na_if(clean_data$d5, 22) # set DK/NA values as NA
-clean_data$d5r = dplyr::na_if(clean_data$d5r, 9) # set DK/NA values as NA
+clean_data$d14 = dplyr::na_if(clean_data$d14, "Refusal (DO NOT READ OUT)") # set DK/NA values as NA
+clean_data$d1 = dplyr::na_if(clean_data$d1, "99 [ACCORDING TO d1r1: REFUSAL]") # set DK/NA values as NA
+clean_data$d1r1 = dplyr::na_if(clean_data$d1r1, "Refusal") # set DK/NA values as NA
+clean_data$d1r2 = dplyr::na_if(clean_data$d1r2, "Refusal") # set DK/NA values as NA
+clean_data$d4 = dplyr::na_if(clean_data$d4, "DK") # set DK/NA values as NA
+clean_data$d4 = dplyr::na_if(clean_data$d4, "Refusal") # set DK/NA values as NA
+clean_data$d4r1 = dplyr::na_if(clean_data$d4r1, "Refusal") # set DK/NA values as NA
+clean_data$d4r1 = dplyr::na_if(clean_data$d4r1, "DK") # set DK/NA values as NA
+clean_data$d4r2 = dplyr::na_if(clean_data$d4r2, "Refusal") # set DK/NA values as NA
+clean_data$d4r2 = dplyr::na_if(clean_data$d4r2, "DK") # set DK/NA values as NA
+clean_data$d5 = dplyr::na_if(clean_data$d5, "Refusal") # set DK/NA values as NA
+clean_data$d5r = dplyr::na_if(clean_data$d5r, "Refusal") # set DK/NA values as NA
+
 
 # keep only complete cases
-clean_data<- clean_data[complete.cases(clean_data), ]
+clean_data = clean_data[complete.cases(clean_data), ]
 
 # see how many cases we have per city now
 clean_data %>% group_by(cities) %>% count() %>% ungroup() %>% summarise(mean(n)) # mean cases per city after cleaning
@@ -35,3 +35,28 @@ clean_data %>% group_by(cities) %>% count() %>% ungroup() %>% summarise(max(n)) 
 raw_data %>% group_by(cities) %>% count() %>% ungroup() %>% summarise(max(n)) # max n of cases per city before cleaning
 clean_data %>% group_by(cities) %>% count() %>% ungroup() %>% summarise(min(n)) # min n of cases per city after cleaning
 raw_data %>% group_by(cities) %>% count() %>% ungroup() %>% summarise(min(n)) # min n of cases per city before cleaning
+
+# rename variables
+clean_data = clean_data %>% rename(
+  public_transport = q1_1,
+  air_quality = q1_10,
+  cc_fighgt = q2_9,
+  finance_household = q3_2,
+  difficulty_bills = d14,
+  train_commute = d21.2,
+  public_transport_commute = d21.5,
+  bycicle_commute = d21.6,
+  walking_commute = d21.7,
+  age = d1,
+  age_4groups = d1r1,
+  age_6groups = d1r2,
+  gender = d2,
+  education = d4,
+  education_13groups = d4r1,
+  education_5groups = d4r2
+)
+
+
+
+
+

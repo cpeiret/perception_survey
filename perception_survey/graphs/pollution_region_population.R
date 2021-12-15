26fdbb1f729408d15e9de3068ed60b22f61b85e1
cpeiret <c.peiret-garcia2@newcastle.ac.uk>
2021-12-15 11:53:04 +0000

    Pollution, population, wealth plot

--- perception_survey/graphs/pollution_region_population.R
+++ perception_survey/graphs/pollution_region_population.R
@@ -23,7 +23,8 @@ city_data = clean_data %>%
   pivot_longer(cols = c(eastern_europe, western_europe, northern_europe, southern_europe), names_to = "regions") %>%
   filter(value == 1) %>%
   select(-value) %>%
-  mutate(population = population * 1000)
+  mutate(population = population * 1000) %>%
+  filter(population != 0)
 
 # pollution vs population
 
@@ -31,8 +32,21 @@ pal = c("#264653", "#2A9D8F", "#E9C46A", "#E76F51")
 
 ggplot(data = city_data, aes(x = gdp_pc, y = pm25_2015)) +
   geom_point(aes(colour = regions, size = population), alpha = 0.7) +
-  scale_color_manual(values = pal) +
-  scale_size(range = c(1, 20))
+  scale_color_manual(values = pal, name = "Region", labels = c("Eastern Europe", "Northern Europe", "Southern Europe", "Western Europe")) +
+  scale_size(range = c(1, 50), name = "Population", labels = scales::comma) +
+  guides(colour = guide_legend(override.aes = list(size = 5)), size = guide_legend(override.aes = list(colour = "grey"))) +
+  theme_minimal() +
+  scale_x_continuous(labels = scales::comma, name = "GDP per capita in euros") +
+  scale_y_continuous(name = "Annual average concentration of PM2.5 particles in μg/m3") +
+  labs(
+    title = "Relationship between urban pollution and GDP per capita and population",
+    subtitle = "There is a clear negative relationship between air pollution and city wealth. There also seems to be a relationship between region and pollution, with cities from Eastern Europe showing higher levels of pollution, and Northern-European cities showing lower levels. 
+The relationship between city size and pollution is not clear, as we see cities of all sizes scattered all over the spectrum of pollution levels."
+  ) +
+  theme(legend.text = element_text(size = 12),
+        legend.title = element_text(size = 16),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12))
 
 
 
